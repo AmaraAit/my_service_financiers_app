985d4dd048fe86cdb14ef4c56e2a118534d6e529
amara <ait_amara@outlook.fr>
2024-12-05 01:33:45 +0100

    version loan update

--- account-service/src/main/java/com/example/demo/web/AccountController.java
+++ account-service/src/main/java/com/example/demo/web/AccountController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.naming.InsufficientResourcesException;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/accounts")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AccountController {
 	
 	IAccountService accountService;
--- loan-service/src/main/java/com/example/demo/web/LoanController.java
+++ loan-service/src/main/java/com/example/demo/web/LoanController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.LoanDto;
@@ -18,6 +19,7 @@ import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/loan")
 public class LoanController {
 	
     private  final ILoanService iLoanService; 
